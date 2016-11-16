fd8114bfb74c335542dd44c9d989fb57aa28ccad
mottig <mottigold@gmail.com>
2016-11-16 21:05:06 +0200

    quantize works. needs args&edge-cases checks

--- ex1/sol1.py
+++ ex1/sol1.py
@@ -101,9 +101,11 @@ def yiq2rgb(imYIQ: np.ndarray) -> np.ndarray:
 
 def get_hist_cdf_and_yiq(im_orig: np.ndarray) -> tuple:
     """
-    help function to get the cdf of an image
+    help function to get the histogram, cdf and YIQ matrix of an image
+    the function also prepare the original image to work with histograms by transferring to [0-255] int.
     :param im_orig: greyscale or RGB float32 image with values in [0, 1]
-    :return: tuple contains: (orig_hist, bin_edges, cdf, yiq_mat)
+    :return: tuple contains: (im, orig_hist, bin_edges, cdf, yiq_mat)
+    im - the original image (or the Y matrix in case of RGB image) as [0-255] integers
     orig_hist - the original images' histogram, in case of a RGB image - the histogram of the Y matrix.
     bin_edges - the edges of the original images' histogram, in case of a RGB image - the histogram of the Y matrix.
     cdf - the cumulative histogram
@@ -114,11 +116,11 @@ def get_hist_cdf_and_yiq(im_orig: np.ndarray) -> tuple:
         yiq_mat = rgb2yiq(im_orig)  # convert to YIQ and take only Y matrix
         im_orig = yiq_mat[:, :, 0]
 
-    im_orig = (im_orig * MAX_PIX_VAL).round().astype(np.uint8)
-    hist_orig, bin_edges = np.histogram(im_orig, MAX_PIX_VAL + 1, [MIN_PIX_VAL, MAX_PIX_VAL])
+    im = (im_orig * MAX_PIX_VAL).round().astype(np.uint8)
+    hist_orig, bin_edges = np.histogram(im, MAX_PIX_VAL + 1, [MIN_PIX_VAL, MAX_PIX_VAL])
     cdf = np.cumsum(hist_orig)
 
-    return hist_orig, bin_edges, cdf, yiq_mat
+    return im, hist_orig, bin_edges, cdf, yiq_mat
 
 
 def histogram_equalize(im_orig: np.ndarray) -> tuple:
@@ -131,9 +133,8 @@ def histogram_equalize(im_orig: np.ndarray) -> tuple:
     hist_eq - is a 256 bin histogram of the equalized image.
     """
     # TODO try catch
-    hist_orig, bin_edges, cdf, yiq_mat = get_hist_cdf_and_yiq(im_orig)
+    im_orig, hist_orig, bin_edges, cdf, yiq_mat = get_hist_cdf_and_yiq(im_orig)
     norm_cdf = np.round(MAX_PIX_VAL * (cdf - min(cdf)) / (max(cdf) - min(cdf)))
-    im_orig = (im_orig * MAX_PIX_VAL).round().astype(np.uint8)
     im_eq = np.interp(im_orig, bin_edges[:-1], norm_cdf)
     hist_eq, bin_edges_eq = np.histogram(im_eq, MAX_PIX_VAL + 1, [MIN_PIX_VAL, MAX_PIX_VAL])
     im_eq = im_eq.astype(np.float32) / MAX_PIX_VAL
@@ -145,68 +146,84 @@ def histogram_equalize(im_orig: np.ndarray) -> tuple:
 
 
 
-def quantize(im_orig: np.ndarray, n_quant: int, n_iter: int) -> np.ndarray:
+def quantize(im_orig: np.ndarray, n_quant: int, n_iter: int) -> tuple:
+    """
+    perform optimal quantization of a given greyscale or RGB image.
+    :param im_orig: greyscale or RGB float32 image with values in [0, 1]
+    :param n_quant: the number of intensities your output im_quant image should have.
+    :param n_iter: the maximum number of iterations of the optimization procedure (may converge earlier).
+    :return: [im_quant, errors_arr] -
+    im_quant - the quantize output image.
+    errors_arr - is an array of the total intensities error for each iteration in the quantization procedure.
+    """
 
     # TODO args check
-    hist_orig, bin_edges, cdf, yiq_mat = get_hist_cdf_and_yiq(im_orig)
     errors_arr = []
+    im_orig, hist_orig, bin_edges, cdf, yiq_mat = get_hist_cdf_and_yiq(im_orig)
 
     # calc initial z
     z_arr = np.zeros(n_quant + 1, int)
     for i in range(1, n_quant):  # start from 1, first val is 0
-        val = (i/n_quant)*max(cdf)
-        z_arr[i] = np.searchsorted(cdf, val)
+        z_arr[i] = np.searchsorted(cdf, (i/n_quant)*max(cdf))
     z_arr[n_quant] = MAX_PIX_VAL  # last val is 255
-    print(z_arr)
+
     q_arr = np.zeros(n_quant, int)
+
+    # start optimization
     for it in range(n_iter):
         curr_err = 0
+
         # calc q and the error of the current iteration
         z_min = 0
-        for i in range(n_quant):  # TODO check the borders, right now each z_i is calculates twice (except 0 and 255)
+        for i in range(n_quant):
             z_max = z_arr[i+1]
-            q_arr[i] = (np.dot(hist_orig[z_arr[i]:z_arr[i+1]+1], np.arange(z_arr[i], z_arr[i+1]+1)) /
+            q_arr[i] = (hist_orig[z_arr[i]:z_arr[i+1]+1].dot(np.arange(z_arr[i], z_arr[i+1]+1)) /
                         np.sum(hist_orig[z_arr[i]:z_arr[i+1]+1])).round().astype(np.uint32)
 
             # calc error:
-            curr_err += sum(hist_orig[z_min:z_max+1] * np.square(np.arange(z_min, z_max+1) - q_arr[i]))
+            curr_err += hist_orig[z_min:z_max+1].dot(np.square(np.arange(z_min, z_max+1) - q_arr[i]))
             z_min = z_max+1
 
+        # calc new z values, the borders (0 and 255) remains the same, so calc only z_i:
+        new_z_arr = np.empty(n_quant - 1, int)
+        for i in range(n_quant-1):  # start from 1, first val is 0
+            new_z_arr[i] = ((q_arr[i] + q_arr[i+1]) / 2).round().astype(np.uint32)
+
         errors_arr.append(curr_err)
-
-        # calc new z values, the borders (0 and 255) remains the same:
-        new_z_arr = np.zeros(n_quant + 1, int)
-        for i in range(1, n_quant):  # start from 1, first val is 0
-            new_z_arr[i] = ((q_arr[i-1] + q_arr[i]) / 2).round().astype(np.uint32)
-        new_z_arr[n_quant] = MAX_PIX_VAL  # last val is 255
-
-        if not np.array_equal(new_z_arr, z_arr):
-            z_arr = new_z_arr.copy()
+        if not np.array_equal(new_z_arr, z_arr[1:-1]):
+            z_arr[1:-1] = new_z_arr
+            print(z_arr)
         else:  # got convergence!
             break
-
 
     # quantise the histogram
     for i in range(n_quant):
         hist_orig[z_arr[i]:z_arr[i+1]+1] = q_arr[i]
-    # TODO solve bug: im_quant in case of RGB should be 2DIM
-    im_quant = np.interp((im_orig*255).round().astype(np.uint32), bin_edges[:-1], hist_orig).astype(np.float32) / MAX_PIX_VAL
+
+    im_quant = np.interp(im_orig, bin_edges[:-1], hist_orig).astype(np.float32) / MAX_PIX_VAL
+
     if yiq_mat is not None:  # im_eq needs to convert to RGB
         yiq_mat[:, :, 0] = im_quant
         im_quant = yiq2rgb(yiq_mat).clip(0, 1)
 
-    return im_quant, errors_arr
+    return im_quant, np.array(errors_arr)
 
-
-res = quantize(read_image("tests/external/Low Contrast.jpg", 2),3,50)
+# TODO dell
+res1 = quantize(read_image("tests/external/monkey.jpg", 1), 3, 50)
+res2 = quantize(read_image("tests/external/monkey.jpg", 2), 3, 50)
+print(res1[1].shape, res2[1].shape)
 f = plt.figure()
-f.add_subplot(1, 2, 1)
-plt.imshow(read_image("tests/external/Low Contrast.jpg", 2), cmap=plt.cm.gray)
-f.add_subplot(1, 2, 2)
-plt.imshow(res[0], cmap=plt.cm.gray)
+f.add_subplot(2, 3, 1)
+plt.imshow(read_image("tests/external/monkey.jpg", 1), cmap=plt.cm.gray)
+f.add_subplot(2, 3, 2)
+plt.imshow(res1[0], cmap=plt.cm.gray)
+f.add_subplot(2, 3, 3)
+plt.plot(res1[1])
+f.add_subplot(2, 3, 4)
+plt.imshow(read_image("tests/external/monkey.jpg", 2), cmap=plt.cm.gray)
+f.add_subplot(2, 3, 5)
+plt.imshow(res2[0], cmap=plt.cm.gray)
+f.add_subplot(2, 3, 6)
+plt.plot(res2[1])
 plt.show()
-
-
-
-
 
