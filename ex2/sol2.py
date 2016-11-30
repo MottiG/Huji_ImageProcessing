d9faa7540ebb8cc87ef677fee31b7eb15d54d1b5
mottig <mottigold@gmail.com>
2016-11-30 15:53:06 +0200

    finish article 1

--- ex2/sol2.py
+++ ex2/sol2.py
@@ -5,9 +5,6 @@ STANDARD = 1
 CONJUGATE = -1
 
 
-def N(x): return x.shape[0]
-
-
 def dft_matrix(n: int, conj_factor: int) -> np.ndarray:
     """
     get a dft matrix based on vandermonde matrix of size nXn
@@ -25,10 +22,10 @@ def DFT(signal: np.ndarray) -> np.ndarray:
     """
     Calc the fourier transform of a given 1D signal, using the formula F(u)=W*f(x)
     for W - DFT matrix (vandermonde matrix of the form w = (-2*pi*i)/N), and f(x) the signal.
-    :param signal: f(x) the signal in his standard domain
+    :param signal: f(x) the signal in space domain
     :return: F(u) - the signal in the frequency domain
     """
-    return dft_matrix(N(signal), STANDARD).dot(signal)
+    return dft_matrix(signal.shape[0], STANDARD).dot(signal)
 
 
 def IDFT(fourier_signal: np.ndarray) -> np.ndarray:
@@ -36,28 +33,38 @@ def IDFT(fourier_signal: np.ndarray) -> np.ndarray:
     Calc the inverse fourier transform of a given 1D fourier signal, using the formula f(x)=W^(-1)*F(u)/N
     for W^(-1) - the inverse DFT matrix of the form w = (2*pi*i)/N, and F(u) the fourier signal.
     :param fourier_signal: F(u) - the signal in the frequency domain
-    :return: f(x) the signal in his standard domain
+    :return: f(x) the signal in space domain
     """
-    n = N(fourier_signal)
-    return dft_matrix(n, CONJUGATE).dot(fourier_signal) / n
+    return dft_matrix(fourier_signal.shape[0], CONJUGATE).dot(fourier_signal) / fourier_signal.shape[0]
 
 
 def DFT2(image: np.ndarray) -> np.ndarray:
     """
-    Calc the fourier transform of a given 2D signal, using the formula F(u,v)=f(x,y)*W
-    for W - DFT matrix (vandermonde matrix of the form w = (-2*pi*i)/N), and f(x,y) the signal.
-    :param image: f(x,y) the 2D signal in his standard domain
+    Calc the fourier transform of a given 2D signal, using the formula F(u,v) = W_m * f(x,y) * W_n
+    for W_n - DFT matrix (vandermonde matrix of the form w = (-2*pi*i)/n), and f(x,y) the signal.
+    :param image: f(x,y) the 2D signal in space domain
     :return: F(u,v) - the 2D signal in the frequency domain
     """
-    return image.dot(dft_matrix(N(image), STANDARD))
+    n = image.shape[1]
+    return DFT(image).dot(dft_matrix(n, STANDARD))
 
 
 def IDFT2(fourier_image: np.ndarray) -> np.ndarray:
     """
-    Calc the inverse fourier transform of a given 2D fourier signal, using the formula f(x,y)=F(u,v)*W^(-1)/N
-    for W^(-1) - the inverse DFT matrix of the form w = (2*pi*i)/N, and F(u,v) the fourier signal.
+    Calc the inverse fourier transform of a given 2D fourier signal, using the formula f(x,y)=(W_m*)*F(u,v)*(W_n*)/M*N
+    for W_m* - the inverse DFT matrix of the form w = (2*pi*i)/m, and F(u,v) the fourier signal.
     :param fourier_image: F(u,v) - the 2D signal in the frequency domain
-    :return: f(x,y) the 2D signal in his standard domain
+    :return: f(x,y) the 2D signal in space domain
     """
-    n = N(fourier_image)
-    return fourier_image.dot(dft_matrix(n, CONJUGATE)) / n
+    n = fourier_image.shape[1]
+    return IDFT(fourier_image).dot(dft_matrix(n, CONJUGATE)) / n
+
+
+def conv_der(im: np.ndarray) -> np.ndarray:
+    """
+    compute the magnitude of image derivatives, using simple convolution with [1 0 -1].
+    :param im: greyscale image of type float32.
+    :return: the magnitude of image derivatives as greyscale image of type float32.
+    """
+    dev_vec = [1, 0, -1]
+
--- ex2/sol2Tests.py
+++ ex2/sol2Tests.py
@@ -1,8 +1,7 @@
 from sol2 import *
 
-papo = np.random.random(32)
-print(papo.ndim)
-# papo = np.copy(papo).reshape(32,1)
+papo = np.random.random(20)
+# papo = np.copy(papo).reshape(20,1)
 
 
 r1 = DFT(papo)
@@ -10,5 +9,5 @@ r2 = np.fft.fft(papo)
 # r1= DFT(papo)
 # r3=DFT(papo2)
 # r2 = (np.fft.fft2(papo2))
-
+print(r1.dtype,r2.dtype)
 print(np.allclose(r1,r2))
