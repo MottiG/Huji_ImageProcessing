eda2ba46ad10ca16c4f0e5e297bda8100cf44415
motti <mottigold@gmail.com>
2016-12-02 01:24:56 +0200

    finish. before deep testing

--- ex2/sol2.py
+++ ex2/sol2.py
@@ -1,8 +1,16 @@
 import numpy as np
-import matplotlib.pyplot as plt
+from scipy import signal as sp_signal
 
 STANDARD = 1
 CONJUGATE = -1
+GAUSSIAN_BASE = np.ones((1, 2), np.float32)  # gaussian kernel base - [1 1] vector
+
+
+def magnitude(x: np.ndarray, y: np.ndarray) -> np.ndarray:
+    """
+    get magnitude of image as float32 dtype
+    """
+    return np.sqrt(np.abs(x)**2 + np.abs(y)**2).astype(np.float32)
 
 
 def dft_matrix(n: int, conj_factor: int) -> np.ndarray:
@@ -51,8 +59,9 @@ def DFT2(image: np.ndarray) -> np.ndarray:
 
 def IDFT2(fourier_image: np.ndarray) -> np.ndarray:
     """
-    Calc the inverse fourier transform of a given 2D fourier signal, using the formula f(x,y)=(W_m*)*F(u,v)*(W_n*)/M*N
-    for W_m* - the inverse DFT matrix of the form w = (2*pi*i)/m, and F(u,v) the fourier signal.
+    Calc the inverse fourier transform of a given 2D fourier signal,
+    using the formula f(x,y)=(W_m*)*F(u,v)*(W_n*)/M*N, for W_m* - the inverse DFT matrix of the
+    form w = (2*pi*i)/m, and F(u,v) the fourier signal.
     :param fourier_image: F(u,v) - the 2D signal in the frequency domain
     :return: f(x,y) the 2D signal in space domain
     """
@@ -66,5 +75,87 @@ def conv_der(im: np.ndarray) -> np.ndarray:
     :param im: greyscale image of type float32.
     :return: the magnitude of image derivatives as greyscale image of type float32.
     """
-    dev_vec = [1, 0, -1]
+    dev_vec = np.array([1, 0, -1]).reshape(3, 1).astype(np.float32)
+    dx = sp_signal.convolve2d(im, dev_vec, 'same')
+    dy = sp_signal.convolve2d(im, dev_vec.T, 'same')
+    return magnitude(dx, dy)
+
+
+def fourier_der(im: np.ndarray) -> np.ndarray:
+    """
+    compute the magnitude of image derivatives, using fourier transform.
+    :param im: greyscale image of type float32.
+    :return: the magnitude of image derivatives as greyscale image of type float32.
+    """
+    fourier_im = np.fft.fftshift(DFT2(im))
+    m, n = fourier_im.shape
+    x_const, y_const = (2j*np.pi)/n, (2j*np.pi)/m
+    u = np.arange(-n/2, n/2, 1)
+    v = np.arange(-m/2, m/2, 1).reshape(m, 1)
+    dx = x_const * np.fft.ifftshift(fourier_im * u)  # C*F(u,v)*u and shift back
+    dy = y_const * np.fft.ifftshift(fourier_im * v)  # same with v
+    inv_dx, inv_dy = IDFT2(dx), IDFT2(dy)
+    return magnitude(inv_dx, inv_dy)
+
+
+def gaussian_kernel(size: int) -> np.ndarray:
+    """
+    create a gaussian kernel
+    :param size: the size of the gaussian kernel in each dimension (an odd integer)
+    :return: gaussian kernel as np.ndarray contains np.float32
+    """
+    if not size % 2: # if size is even number
+        raise Exception("kernel size must be odd number")
+
+    kernel = GAUSSIAN_BASE
+    for i in range(size-2):
+        kernel = sp_signal.convolve(kernel, GAUSSIAN_BASE)
+    kernel = sp_signal.convolve2d(kernel, kernel.T,) / np.sum(kernel)  # change 1D to 2D kernel and normalize
+    return kernel
+
+
+def blur_spatial(im: np.ndarray, kernel_size: int) -> np.ndarray:
+    """
+    perform image blurring using 2D convolution between the image im and a gaussian kernel g.
+    :param im: the input image to be blurred (greyscale float32 image).
+    :param kernel_size: the size of the gaussian kernel in each dimension (an odd integer)
+    :return: blurry image (greyscale float32 image).
+    """
+    if min(im.shape) < kernel_size: # if kernel_size smaller than min(dimensions) of the image
+        raise Exception("kernel_size must be smaller or equal to the smallest dimension of the image")
+
+    g = gaussian_kernel(kernel_size)
+    blur_im = sp_signal.convolve2d(im, g, 'same')
+    return blur_im
+
+
+def blur_fourier(im: np.ndarray, kernel_size: int) -> np.ndarray:
+    """
+    perform image blurring with gaussian kernel in Fourier space.
+    :param im: the input image to be blurred (greyscale float32 image).
+    :param kernel_size: the size of the gaussian kernel in each dimension (an odd integer)
+    :return: blurry image (greyscale float32 image).
+    """
+    if min(im.shape) < kernel_size:  # if kernel_size bigger than min(dimensions) of the image
+        raise Exception("kernel_size must be smaller or equal to the smallest dimension of the image")
+
+    # create padded kernel with equal dimensions to the given image:
+    m, n = im.shape
+    g = np.zeros((m, n), np.float32)
+    kernel = gaussian_kernel(kernel_size)
+    min_row_idx = int(np.floor(m/2) - np.floor(kernel_size/2))
+    max_row_idx = int(np.floor(m/2) + np.floor(kernel_size/2)) + 1
+    min_col_idx = int(np.floor(n/2) - np.floor(kernel_size/2))
+    max_col_idx = int(np.floor(n / 2) + np.floor(kernel_size / 2)) + 1
+    g[min_row_idx:max_row_idx, min_col_idx:max_col_idx] = kernel  # put the kernel in center of g
+    g = np.fft.ifftshift(g)  # move center of g to (0,0)
+
+    # apply the filter and take only real part:
+    blur_im = np.real(IDFT2(DFT2(im) * (DFT2(g)))).astype(np.float32)
+    return blur_im
+
+
+
+
+
 
--- ex2/sol2Tests.py
+++ ex2/sol2Tests.py
@@ -1,13 +1,21 @@
+from sol1 import *
 from sol2 import *
 
-papo = np.random.random(20)
-# papo = np.copy(papo).reshape(20,1)
+im = read_image("ex1/tests/external/jerusalem.jpg", 1)
 
+# r1 = DFT2(im)
+# r1 = np.log(1 +np.abs(np.fft.fftshift(r1)))
+#
+r1 = blur_spatial(im, 25)
+r2 = blur_fourier(im, 25)
 
-r1 = DFT(papo)
-r2 = np.fft.fft(papo)
-# r1= DFT(papo)
-# r3=DFT(papo2)
-# r2 = (np.fft.fft2(papo2))
-print(r1.dtype,r2.dtype)
-print(np.allclose(r1,r2))
+f = plt.figure()
+a = f.add_subplot(1,2,1)
+a.title.set_text('blur_spatial')
+plt.imshow(r1, cmap=plt.cm.gray)
+b = f.add_subplot(1,2,2)
+b.title.set_text('blur_fourier')
+plt.imshow(r2, cmap=plt.cm.gray)
+# f.add_subplot(1,3,3)
+# plt.imshow(r2, cmap=plt.cm.gray)
+plt.show()
