55bd9fb0d87bd6775b10f2187af6b53d4e088fe3
motti <mottigold@gmail.com>
2017-01-11 19:03:20 +0200

    finish 3.1

--- ex4/sol4.py
+++ ex4/sol4.py
@@ -6,7 +6,9 @@ DER_FILTER = np.array([1, 0, -1], np.float32).reshape(1, 3)
 BLUR_SIZE = 3
 K = 0.04
 N = M = 4  # defaults for spread_out_corners n and m
-
+RADIUS = 3
+DEFAULT_DESC_RAD = 3
+DEFAULT_MIN_SCORE = 0.5
 
 def harris_corner_detector(im: np.ndarray) -> np.ndarray:
     """
@@ -27,11 +29,42 @@ def harris_corner_detector(im: np.ndarray) -> np.ndarray:
 
 
 def sample_descriptor(im: np.ndarray, pos: np.ndarray, desc_rad: int) -> np.ndarray:
+    """
+    sample a given image with simplified version of the MOPS descriptor
+    :param im: grayscale image to sample within.
+    :param pos: An array with shape (N,2) of [x,y] positions to sample descriptors in im
+    :param desc_rad: "Radius" of descriptors to compute
+    :return: A 3D array with shape (K,K,N) containing the ith descriptor at desc[:,:,i]
+    """
     n = pos.shape[0]
-    desc = np.zeros((1 + 2 * desc_rad, 1 + 2 * desc_rad, n), np.float32)
+    desc = np.empty((1 + 2*desc_rad, 1 + 2*desc_rad, n), np.float32)
     for i in range(n):
         grid = np.mgrid[pos[i, 0] - desc_rad: pos[i, 0] + desc_rad + 1,
-               pos[i, 1] - desc_rad: pos[i, 1] + desc_rad + 1]
-        win = interpolation.map_coordinates(im, grid, order=1, prefilter=False)
+                        pos[i, 1] - desc_rad: pos[i, 1] + desc_rad + 1]
+        desc_win = interpolation.map_coordinates(im, grid, order=1, prefilter=False)
+        win_mean = np.mean(desc_win)
+        win_std = np.linalg.norm(desc_win - win_mean)
+        if win_std:
+            desc_win = (desc_win - win_mean) / win_std
+        else:
+            desc_win[:] = 0
+        desc[:, :, i] = desc_win
+
+    return desc
+
+
+def find_features(pyr: list) -> np.ndarray:
+    """
+    get simplified version of the MOPS descriptors from the given pyramid and the positions of them
+    :param pyr: Gaussian pyramid of a grayscale image having 3 levels
+    :return: the positions of the descriptors and array of them [with shape (1+2*desc_rad,1+2*desc_rad,N)]
+    """
+    pos = spread_out_corners(pyr[0], N, M, RADIUS)
+    desc = sample_descriptor(pyr[2], pos/4, DEFAULT_DESC_RAD)
+    return pos, desc
+
+
+
+
 
 
--- ex4/sol4Tests.py
+++ ex4/sol4Tests.py
@@ -2,8 +2,11 @@ from sol4 import *
 import matplotlib.pyplot as plt
 
 im = read_image('external/backyard1.jpg', 1)
+pyr = build_gaussian_pyramid(im, 3, 3)[0]
+pos, desc = find_features(pyr)
+a=3
 # pos = harris_corner_detector(im)
-points = spread_out_corners(im, 4, 4, 7)
-plt.imshow(im, cmap=plt.cm.gray)
-plt.scatter(points[:, 0], points[:, 1], edgecolors='r')
-plt.show()
+# points = spread_out_corners(im, 4, 4, 7)
+# plt.imshow(im, cmap=plt.cm.gray)
+# plt.scatter(points[:, 0], points[:, 1], edgecolors='r')
+# plt.show()
