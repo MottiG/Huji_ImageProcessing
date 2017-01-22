d88b9ddf740f274318929c70af3090b0c6f01fed
mottig <mottigold@gmail.com>
2017-01-22 17:49:04 +0200

    work on train model

--- ex5/sol5.py
+++ ex5/sol5.py
@@ -1,9 +1,14 @@
-from keras.layers import Input , Dense, Convolution2D, Activation
+from keras.layers import Input , merge, Convolution2D, Activation
 from keras.models import Model
+from keras.optimizers import adam
 import numpy as np
 
 SUBVAL = 0.5  # value to subtract from each image
 KERNEL_SIZE = 3
+NUM_OF_BLOCKS = 5  # default number of rsblocks of the network
+PRECENT_OF_VALID = 20
+BETA_2 = 0.9  # for adam optimizer
+
 
 # # # # # # # # # # START OF COPY&PASTE FROM EX1 - THE PRESUBMIT USES THE READ_IMAGE FUNCTION # # # # # # # #
 from scipy.misc import imread
@@ -47,15 +52,15 @@ def load_dataset(filenames: list, batch_size: int, corruption_func: callable, cr
             else:
                 im = read_image(im_name, GREYSCALE)
                 im_dict[im_name] = im
-            # crop_im = corruption_func(im)
+            crop_im = corruption_func(im)
             # take patch
             max_patch_y, max_patch_x = im.shape[0] - crop_rows, im.shape[1] - crop_cols
             patch_y = np.random.randint(max_patch_y + 1)
             patch_x = np.random.randint(max_patch_x + 1)
             im_patch = im[patch_y: patch_y+crop_rows, patch_x: patch_x+crop_cols] - SUBVAL
-            # crop_patch = crop_im[patch_y: patch_y+crop_rows, patch_x: patch_x+crop_cols] - SUBVAL
+            crop_patch = crop_im[patch_y: patch_y+crop_rows, patch_x: patch_x+crop_cols] - SUBVAL
             target_batch[i, ...] = im_patch[np.newaxis, ...]
-            # source_batch[i, ...] = crop_patch[np.newaxis, ...]
+            source_batch[i, ...] = crop_patch[np.newaxis, ...]
 
         yield source_batch, target_batch
 
@@ -67,11 +72,46 @@ def resblock(input_tensor: np.ndarray, num_channels: int) -> np.ndarray:
     :param num_channels: number of channels for each of the convolutional layers
     :return: symbolic output tensor of the layer conguration
     """
-    # inpt = Input(shape=input_tensor.shape)  # TODO check if needed or send input directly to conv
-    first_conv =  Convolution2D(num_channels , KERNEL_SIZE, KERNEL_SIZE, border_mode ='same ')(input_tensor)
+    first_conv =  Convolution2D(num_channels , KERNEL_SIZE, KERNEL_SIZE, border_mode ='same')(input_tensor)
     actv = Activation('relu')(first_conv)
-    second_conv = Convolution2D(num_channels, KERNEL_SIZE, KERNEL_SIZE, border_mode='same ')(actv)
-    output_tensor = input_tensor + second_conv
+    second_conv = Convolution2D(num_channels, KERNEL_SIZE, KERNEL_SIZE, border_mode='same')(actv)
+    output_tensor = merge([input_tensor, second_conv], mode ='sum')
     return output_tensor
+
+
+def build_nn_model(height: int, width: int, num_channels: int) -> Model:
+    """
+
+    :param height: height of tensor output of the model
+    :param width: width of tensor output of the model
+    :param num_channels: number of channels for each of the convolutional layers except the last one
+    :return: the network model
+    """
+    inpt = Input(shape=(GREYSCALE, height, width))
+    conv = Convolution2D(num_channels, KERNEL_SIZE, KERNEL_SIZE, border_mode='same')(inpt)
+    actv = Activation('relu')(conv)
+    block_inpt = actv
+    for i in range(NUM_OF_BLOCKS):
+        block_inpt = resblock(block_inpt, num_channels=num_channels)  # TODO check if needed another variable as "out"
+    mrg = merge([actv, block_inpt], mode ='sum')
+    last_conv = Convolution2D(1, KERNEL_SIZE, KERNEL_SIZE, border_mode='same')(mrg)
+    return Model(inpt, last_conv)
+
+
+def train_model(model: Model, images: list, corruption_func: callable, batch_size: int,
+                samples_per_epoch: int, num_epochs: int, num_valid_samples: int):
+    """
+    train the model to recognize corrupted images
+    :param model: a general neural network model for image restoration.
+    :param images: a list of files paths pointing to image files.
+    :param corruption_func: A function receiving a numpy's array representation of an image as a single
+    argument, and returns a randomly corrupted version of the input image.
+    :param batch_size: the size of the batch of examples for each iteration of SGD.
+    :param samples_per_epoch: The number of samples in each epoch (actual samples, not batches!).
+    :param num_epochs: The number of epochs for which the optimization will run.
+    :param num_valid_samples: The number of samples in the validation set to test on after every epoch.
+    :return:
+    """
+
 
 
