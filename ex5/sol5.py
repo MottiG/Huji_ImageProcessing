c83dff835c199b2c47a278abb2f4986b04450f53
motti <mottigold@gmail.com>
2017-01-23 01:33:43 +0200

    look like done. have some questions for stuff, as TODOs.

--- ex5/sol5.py
+++ ex5/sol5.py
@@ -1,17 +1,11 @@
-from keras.layers import Input , merge, Convolution2D, Activation
+from keras.layers import Input, merge, Convolution2D, Activation
 from keras.models import Model
 from keras.optimizers import adam
 import numpy as np
+from scipy.ndimage import filters
+import sol5_utils
 
-NORM_VAL = 0.5  # value to subtract from each image
-KERNEL_SIZE = 3
-NUM_OF_BLOCKS = 5  # default number of rsblocks of the network
-LOSS_FUNC = 'mean_square_error'
-PERCENT_OF_TRAIN = 0.8
-BETA_2 = 0.9  # for adam optimizer
-
-
-# # # # # # # # # # START OF COPY&PASTE FROM EX1 - THE PRESUBMIT USES THE READ_IMAGE FUNCTION # # # # # # # #
+# # # # # # # # # # # # #  START OF COPY&PASTE FROM EX1 - FOR READ_IMAGE FUNCTION # # # # # # # # # # # # # #
 from scipy.misc import imread
 from skimage.color import rgb2gray
 GREYSCALE, COLOR, RGBDIM = 1, 2, 3
@@ -28,8 +22,42 @@ def read_image(filename: str, representation: int) -> np.ndarray:
     return im.astype(np.float32) / MAX_PIX_VAL
 # # # # # # # # # # # # # # # # # # # # END OF COPY&PASTE FROM EX1 # # # # # # # # # # # # # # # # # # # # # #
 
+NORM_VAL = 0.5  # value subtracted from each image to normalize mean
+CONV_KERNEL_SIZE = 3  # default size of the kernels using as convolution layer
+NUM_OF_BLOCKS = 5  # default number of resblocks of the network
+LOSS_FUNC = 'mean_squared_error'
+PERCENT_OF_TRAIN = 0.8
+BETA_2 = 0.9  # for 'adam' optimizer
 
-def load_dataset(filenames: list, batch_size: int, corruption_func: callable, crop_size: tuple) -> tuple:
+# denoising parameters:
+DENOIS_PATCH_SIZE = 24
+DENOIS_CHANNELS = 48
+DENOIS_MIN_SIG = 0.0
+DENOIS_MAX_SIG = 0.2
+DENOIS_BATCH_SIZE = 100
+DENOIS_TEST_BATCH_SIZE = 10
+DENOIS_EPOCH_SIZE = 10000
+DENOIS_TEST_EPOCH_SIZE = 30
+DENOIS_EPOCH_NB = 5
+DENOIS_TEST_EPOCH_NB = 2
+DENOIS_VALID_SIZE = 1000
+DENOIS_TEST_VALID_SIZE = 30
+
+# deblurring parameters:
+DEBLUR_PATCH_SIZE = 16
+DEBLUR_CHANNELS = 32
+DEBLUR_KERNEL_SIZE = 7
+DEBLUR_BATCH_SIZE = 100
+DEBLUR_TEST_BATCH_SIZE = 10
+DEBLUR_EPOCH_SIZE = 10000
+DEBLUR_TEST_EPOCH_SIZE = 30
+DEBLUR_EPOCH_NB = 10
+DEBLUR_TEST_EPOCH_NB = 2
+DEBLUR_VALID_SIZE = 1000
+DEBLUR_TEST_VALID_SIZE = 30
+
+
+def load_dataset(filenames: list, batch_size: int, corruption_func: callable, crop_size: tuple):
     """
     get generator to generate random data, see ex. pdf for more details.
     :param filenames: A list of file names of clean images
@@ -40,11 +68,10 @@ def load_dataset(filenames: list, batch_size: int, corruption_func: callable, cr
     :return: Python's generator object which outputs random tuples of the form (source_batch, target_batch)
     """
     max_num = len(filenames)
-    crop_rows, crop_cols = crop_size
-    im_dict = {}  # TODO check if should be here or inside the while
+    crop_rows, crop_cols = crop_size[2], crop_size[3] # TODO check if this is correct or we have anothr problem cause crop_size to be 4-tuple
+    im_dict = {}
 
     while True:
-
         target_batch = source_batch = np.empty((batch_size, GREYSCALE, crop_rows, crop_cols), np.float32)
         for i in range(batch_size):
             im_name = filenames[np.random.randint(max_num)]
@@ -71,11 +98,11 @@ def resblock(input_tensor: np.ndarray, num_channels: int) -> np.ndarray:
     A residual block as described in ex. pdf
     :param input_tensor: a symbolic input tensor
     :param num_channels: number of channels for each of the convolutional layers
-    :return: symbolic output tensor of the layer conguration
+    :return: symbolic output tensor of the layer configuration
     """
-    layer = Convolution2D(num_channels , KERNEL_SIZE, KERNEL_SIZE, border_mode ='same')(input_tensor)
+    layer = Convolution2D(num_channels, CONV_KERNEL_SIZE, CONV_KERNEL_SIZE, border_mode ='same')(input_tensor)
     layer = Activation('relu')(layer)
-    layer = Convolution2D(num_channels, KERNEL_SIZE, KERNEL_SIZE, border_mode='same')(layer)
+    layer = Convolution2D(num_channels, CONV_KERNEL_SIZE, CONV_KERNEL_SIZE, border_mode='same')(layer)
     output_tensor = merge([input_tensor, layer], mode ='sum')
     return output_tensor
 
@@ -89,13 +116,13 @@ def build_nn_model(height: int, width: int, num_channels: int) -> Model:
     :return: the network model
     """
     inpt = Input(shape=(GREYSCALE, height, width))
-    layer = Convolution2D(num_channels, KERNEL_SIZE, KERNEL_SIZE, border_mode='same')(inpt)
+    layer = Convolution2D(num_channels, CONV_KERNEL_SIZE, CONV_KERNEL_SIZE, border_mode='same')(inpt)
     layer = Activation('relu')(layer)
     block_inpt = layer
     for i in range(NUM_OF_BLOCKS):
-        block_inpt = resblock(block_inpt, num_channels=num_channels)  # TODO check if needed another variable as "out"
+        block_inpt = resblock(block_inpt, num_channels=num_channels)
     layer = merge([layer, block_inpt], mode ='sum')
-    last_conv = Convolution2D(1, KERNEL_SIZE, KERNEL_SIZE, border_mode='same')(layer)
+    last_conv = Convolution2D(1, CONV_KERNEL_SIZE, CONV_KERNEL_SIZE, border_mode='same')(layer)
     return Model(inpt, last_conv)
 
 
@@ -116,7 +143,7 @@ def train_model(model: Model, images: list, corruption_func: callable, batch_siz
     train_size = int(PERCENT_OF_TRAIN*len(images))
     train_set = load_dataset(images[:train_size], batch_size, corruption_func, model.input_shape)
     valid_set = load_dataset(images[train_size:], batch_size, corruption_func, model.input_shape)
-    model.compile(adam(beta_2=BETA_2), LOSS_FUNC)
+    model.compile(loss=LOSS_FUNC, optimizer=adam(beta_2=BETA_2))
     model.fit_generator(generator=train_set, samples_per_epoch=samples_per_epoch, nb_epoch=num_epochs,
                         validation_data=valid_set, nb_val_samples=num_valid_samples)
 
@@ -139,9 +166,11 @@ def restore_image(corrupted_image: np.ndarray, base_model: Model, num_channels: 
 
 def add_gaussian_noise(image: np.ndarray, min_sigma: float, max_sigma: float) -> np.ndarray:
     """
-    add to every pixel of the input image a zero-mean gaussian random variable with standard deviation equal to sigma
+    add to every pixel of the input image a zero-mean gaussian random variable with standard deviation equal
+    to sigma
     :param image: a grayscale image with values in the [0, 1] range of type float32.
-    :param min_sigma:  a non-negative scalar value representing the minimal variance of the gaussian distribution.
+    :param min_sigma:  a non-negative scalar value representing the minimal variance of the gaussian
+    distribution.
     :param max_sigma:  a non-negative scalar value larger than or equal to min_sigma, representing the maximal
     variance of the gaussian distribution.
     :return: corrupted image as np.ndarray with values in the [0, 1] range of type float32
@@ -152,5 +181,64 @@ def add_gaussian_noise(image: np.ndarray, min_sigma: float, max_sigma: float) ->
     return image.clip(0, 1)
 
 
+def learn_denoising_model(quick_mode: bool=False) -> tuple:
+    """
+    train a network to fix noising images
+    :param quick_mode: if true, train with test-parameters to save time
+    :return: tuple contains a trained denoising model, and the number of channels used in its construction
+    """
+    im_list = sol5_utils.images_for_denoising()
+    num_channels = DENOIS_CHANNELS
+    model = build_nn_model(height=DENOIS_PATCH_SIZE, width=DENOIS_PATCH_SIZE, num_channels=num_channels)
+    train_model(model = model,
+                images = im_list,
+                corruption_func = lambda x: add_gaussian_noise(x, DENOIS_MIN_SIG, DENOIS_MAX_SIG),
+                batch_size = DENOIS_BATCH_SIZE if not quick_mode else DENOIS_TEST_BATCH_SIZE,
+                samples_per_epoch = DENOIS_EPOCH_SIZE if not quick_mode else DENOIS_TEST_EPOCH_SIZE,
+                num_epochs = DENOIS_EPOCH_NB if not quick_mode else DENOIS_TEST_EPOCH_NB,
+                num_valid_samples = DENOIS_VALID_SIZE if not quick_mode else DENOIS_TEST_VALID_SIZE)
+    return model, num_channels
 
 
+def add_motion_blur(image: np.ndarray, kernel_size: int, angle: float) -> np.ndarray:
+    """
+    simulate motion blurring on a given image
+    :param image: a grayscale image with values in the [0, 1] range
+    :param kernel_size: an odd integer specifying the size of the kernel
+    :param angle: an angle in radians in the range [0, PI)
+    :return: motion blurred image
+    """
+    kernel = sol5_utils.motion_blur_kernel(kernel_size, angle)
+    image = filters.convolve(image, kernel, mode='mirror')
+    return image
+
+
+def random_motion_blur(image: np.ndarray, list_of_kernel_sizes: list) -> np.ndarray:
+    """
+    simulate random motion blurring on a given image
+    :param image: a grayscale image with values in the [0, 1] range
+    :param list_of_kernel_sizes: a list of odd integers
+    :return: randomly motion blurred image
+    """
+    angel = np.pi*np.random.random_sample()
+    kernel_size = list_of_kernel_sizes[np.random.randint(len(list_of_kernel_sizes))]
+    return add_motion_blur(image, kernel_size, angel)
+
+
+def learn_deblurring_model(quick_mode: bool=False) -> tuple:
+    """
+    train a network to fix blurring images
+    :param quick_mode: if true, train with test-parameters to save time
+    :return: tuple contains a trained deblurring model, and the number of channels used in its construction
+    """
+    im_list = sol5_utils.images_for_deblurring()
+    num_channels = DEBLUR_CHANNELS
+    model = build_nn_model(height=DEBLUR_PATCH_SIZE, width=DEBLUR_PATCH_SIZE, num_channels=num_channels)
+    train_model(model = model,
+                images = im_list,
+                corruption_func = lambda x: random_motion_blur(x, [DEBLUR_KERNEL_SIZE]), # TODO check how kernel should be 7 if its list of numbers?!
+                batch_size = DEBLUR_BATCH_SIZE if not quick_mode else DEBLUR_TEST_BATCH_SIZE,
+                samples_per_epoch = DEBLUR_EPOCH_SIZE if not quick_mode else DEBLUR_TEST_EPOCH_SIZE,
+                num_epochs = DEBLUR_EPOCH_NB if not quick_mode else DEBLUR_TEST_EPOCH_NB,
+                num_valid_samples = DEBLUR_VALID_SIZE if not quick_mode else DEBLUR_TEST_VALID_SIZE)
+    return model, num_channels
--- ex5/tests.py
+++ ex5/tests.py
@@ -1,7 +1,5 @@
 from sol5 import *
 from sol5_utils import *
 
-imlist = images_for_denoising()
-bs = 5
-ls = load_dataset(imlist, bs, type, (4,4))
-tup = next(ls)
+model, channels = learn_denoising_model(False)
+model.save_weights('donis_weights.h5')
