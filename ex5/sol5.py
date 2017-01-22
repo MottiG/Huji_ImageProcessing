004882642e8c721fd4a7141d89c69bdbb738afc9
motti <mottigold@gmail.com>
2017-01-22 15:34:04 +0200

    added resblock func

--- ex5/sol5.py
+++ ex5/sol5.py
@@ -1,8 +1,9 @@
-from keras.models import Sequential
-import tensorflow as tf
+from keras.layers import Input , Dense, Convolution2D, Activation
+from keras.models import Model
 import numpy as np
 
 SUBVAL = 0.5  # value to subtract from each image
+KERNEL_SIZE = 3
 
 # # # # # # # # # # START OF COPY&PASTE FROM EX1 - THE PRESUBMIT USES THE READ_IMAGE FUNCTION # # # # # # # #
 from scipy.misc import imread
@@ -34,14 +35,19 @@ def load_dataset(filenames: list, batch_size: int, corruption_func: callable, cr
     """
     max_num = len(filenames)
     crop_rows, crop_cols = crop_size
+    im_dict = {}  # TODO check if should be here or inside the while
+
     while True:
 
         target_batch = source_batch = np.empty((batch_size, GREYSCALE, crop_rows, crop_cols), np.float32)
         for i in range(batch_size):
-            idx = np.random.randint(max_num)
-            im = read_image(filenames[idx], GREYSCALE)
+            im_name = filenames[np.random.randint(max_num)]
+            if im_name in im_dict:
+                im = im_dict[im_name]
+            else:
+                im = read_image(im_name, GREYSCALE)
+                im_dict[im_name] = im
             # crop_im = corruption_func(im)
-
             # take patch
             max_patch_y, max_patch_x = im.shape[0] - crop_rows, im.shape[1] - crop_cols
             patch_y = np.random.randint(max_patch_y + 1)
@@ -54,6 +60,18 @@ def load_dataset(filenames: list, batch_size: int, corruption_func: callable, cr
         yield source_batch, target_batch
 
 
-
+def resblock(input_tensor: np.ndarray, num_channels: int) -> np.ndarray:
+    """
+    A residual block as described in ex. pdf
+    :param input_tensor: a symbolic input tensor
+    :param num_channels: number of channels for each of the convolutional layers
+    :return: symbolic output tensor of the layer conguration
+    """
+    # inpt = Input(shape=input_tensor.shape)  # TODO check if needed or send input directly to conv
+    first_conv =  Convolution2D(num_channels , KERNEL_SIZE, KERNEL_SIZE, border_mode ='same ')(input_tensor)
+    actv = Activation('relu')(first_conv)
+    second_conv = Convolution2D(num_channels, KERNEL_SIZE, KERNEL_SIZE, border_mode='same ')(actv)
+    output_tensor = input_tensor + second_conv
+    return output_tensor
 
 
